abf5830db8feeffbd024072cca644c8569908dfb
takashikatagiri <99312380+takashikatagiri@users.noreply.github.com>
2022-03-24 14:28:08 +0900

    2nd commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          git push origin master
         </p>
         <a
           className="App-link"
